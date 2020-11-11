ec9e8c0f1505456a3631a5655eec07678199cfce
gerFid <germanFid@users.noreply.github.com>
2020-11-11 20:01:00 +0300

    Current version

---
+++ @ace/meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 463939057;
+name = "ace";
+timestamp = 5248917642997072322;
--- @gerfidAce/meta.cpp
+++
@@ -1,4 +0,0 @@
-protocol = 1;
-publishedid = 463939057;
-name = "ace";
-timestamp = 5248917642997072322;
